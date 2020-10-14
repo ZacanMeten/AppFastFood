f91d8a0713cc2b5e32fec6a773901bb179c95c65
Anthony H <reanthony12@gmail.com>
2020-10-14 15:17:57 -0500

    Implementacion de la viste LOGIN

--- src/main/java/com/moherdi/fastfood_app/SecurityConfigWeb.java
+++ src/main/java/com/moherdi/fastfood_app/SecurityConfigWeb.java
@@ -34,6 +34,19 @@ public class SecurityConfigWeb extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeRequests()
+                    .antMatchers("/login", "/css/**", "/img/**", "/js/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll()
+                    .and()
+                .httpBasic();
     }
 }
