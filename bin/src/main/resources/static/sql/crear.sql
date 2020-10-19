91ffbe8936e7293184646ef792ae6bc9875d7b18
maricielo <maricielo.ms.96@gmail.com>
2020-10-19 18:25:00 -0500

    Esta es mi prueba para subir al Git --Marixd
     Please enter the commit message for your changes. Lines starting

---
+++ bin/src/main/resources/static/sql/crear.sql
@@ -0,0 +1,89 @@
+USE master
+GO
+
+DROP DATABASE BDFastandFood
+GO
+
+CREATE DATABASE BDFastandFood
+GO
+USE BDFastandFood
+GO
+
+/*
+DROP TABLE DETALLE_PED;
+DROP TABLE PEDIDOS;
+DROP TABLE CLIENTES;
+DROP TABLE STAFF;
+DROP TABLE USUARIOS;
+DROP TABLE PRODUCTOS;
+*/
+
+CREATE TABLE USUARIOS(
+	[id_user] int IDENTITY(1,1) PRIMARY KEY,
+	[nombre] varchar(30) NOT NULL UNIQUE,
+	[contrasenia] varchar(100) NOT NULL
+)
+GO
+CREATE TABLE CLIENTES(
+	[id_cli] [int] IDENTITY(1,1) PRIMARY KEY,
+	[nombres] [varchar](30) NOT NULL,
+	[apellidos] [varchar](35) NOT NULL,
+	[edad] [int] NOT NULL,
+	[correo] [varchar](45) NOT NULL,
+	[direccion] [varchar](50) NOT NULL,
+	[id_user] [int] Foreign key REFERENCES USUARIOS(id_user)
+)
+CREATE TABLE STAFF(
+	[id_staff] [int] IDENTITY(1,1) PRIMARY KEY,
+	[nombres] [varchar](30) NOT NULL,
+	[apellidos] [varchar](35) NOT NULL,
+	[correo] [varchar](35) NOT NULL,
+	[rol] [varchar](30) NOT NULL,
+	[id_user] [int] FOREIGN KEY REFERENCES USUARIOS(id_user)
+)
+
+CREATE TABLE PRODUCTOS(
+	id_producto int IDENTITY(1,1) primary key,
+	nombre varchar(30) not null,
+	precio_unit decimal(7,2) not null,
+	dsct_prc int NOT NULL CHECK (dsct_prc <= 100) DEFAULT(0),
+	image_data NTEXT
+)
+
+CREATE TABLE PEDIDOS(
+	[id_pedido] [int] IDENTITY(1,1) PRIMARY KEY,
+	[id_cli] [int] FOREIGN KEY REFERENCES CLIENTES(id_cli),
+	[fecha_emi] datetime DEFAULT GETDATE(),
+	[estado] [varchar](10) NOT NULL,
+	[precio_total] decimal(8,2) NOT NULL,
+	[id_staff] [int] FOREIGN KEY REFERENCES STAFF(id_staff)
+)
+
+CREATE TABLE DETALLE_PED(
+	[id_pedido] [int] FOREIGN KEY REFERENCES PEDIDOS(id_pedido) NOT NULL,
+	[id_prod] [int] FOREIGN KEY REFERENCES PRODUCTOS(id_producto) NOT NULL,
+	[cantidad] [int] NOT NULL,
+	[precio] [decimal](8, 2) NOT NULL
+)
+
+/** inserts **/
+/*
+INSERT INTO USUARIOS VALUES ('anthony','anthony12'),
+			('maricielo','maricielo12'),
+			('carlos','carlos12')
+GO
+*/
+
+INSERT INTO PRODUCTOS VALUES
+           ('Tres Leches', 15.9, 0, null),
+		   ('Torta Helada', 13.5, 0, null),
+           ('Torta de Matrimonio', 240.25, 22, null),
+		   ('Soda Dietetica', 10.34, 0, null),
+		   ('Pastel de Pistacho', 24.5, 20, null),
+		   ('Ratatouille', 22.5, 10, null)
+GO
+
+
+/** SELECTS **/
+SELECT * FROM USUARIOS;
+SELECT * FROM PRODUCTOS;
